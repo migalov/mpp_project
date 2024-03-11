3a60b6a65d6524b1edbd946de22df4b00df8a555
Michel Muniagiseny <mafri.michel@gmail.com>
2024-03-11 08:47:13 +0300

    Start project

--- backend/types/generated/components.d.ts
+++ backend/types/generated/components.d.ts
@@ -1,5 +1,22 @@
 import type { Schema, Attribute } from '@strapi/strapi';
 
+export interface MenuItemMenu extends Schema.Component {
+  collectionName: 'components_menu_item_menus';
+  info: {
+    displayName: 'Item Menu';
+    icon: 'link';
+  };
+  attributes: {
+    label: Attribute.String;
+    link: Attribute.String;
+    previews: Attribute.Media;
+  };
+}
+
 declare module '@strapi/types' {
-  export module Shared {}
+  export module Shared {
+    export interface Components {
+      'menu.item-menu': MenuItemMenu;
+    }
+  }
 }
